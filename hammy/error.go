5533eb347d8ee2efb9f4bbf3e6852b2384da7bbb
Nathan Fisher <nfisher@junctionbox.ca>
2026-02-18 10:12:35 -0500

    hammy: add ErrorIs and ErrorAs assertions

--- hammy/error.go
+++ hammy/error.go
@@ -1,9 +1,19 @@
 package hammy
 
+import "errors"
+
 func NilError(err error) AssertionMessage {
 	return Assert(err == nil, "got <%v>, want nil error", err)
 }
 
 func Error(err error) AssertionMessage {
 	return Assert(err != nil, "got %v, want error", err)
+}
+
+func ErrorIs(err error, target error) AssertionMessage {
+	return Assert(errors.Is(err, target), "got <%v>, want error matching <%v>", err, target)
+}
+
+func ErrorAs[T any](err error, target *T) AssertionMessage {
+	return Assert(errors.As(err, target), "got <%v>, want error assignable to <%T>", err, target)
 }
--- hammy/error_test.go
+++ hammy/error_test.go
@@ -2,11 +2,20 @@ package hammy_test
 
 import (
 	"errors"
+	"fmt"
 	"testing"
 
 	"github.com/gogunit/gunit/eye"
 	hammy "github.com/gogunit/gunit/hammy"
 )
+
+type customError struct {
+	message string
+}
+
+func (e *customError) Error() string {
+	return e.message
+}
 
 func Test_is_error_success(t *testing.T) {
 	var err = errors.New("hello world")
@@ -28,4 +37,36 @@ func Test_nil_error_failure(t *testing.T) {
 	var err = errors.New("hello world")
 	hammy.New(aSpy).Is(hammy.NilError(err))
 	aSpy.HadErrorContaining(t, "got <hello world>, want nil error")
+}
+
+func Test_error_is_success(t *testing.T) {
+	target := errors.New("boom")
+	err := fmt.Errorf("wrapped: %w", target)
+
+	hammy.New(t).Is(hammy.ErrorIs(err, target))
+}
+
+func Test_error_is_failure(t *testing.T) {
+	aSpy := eye.Spy()
+	target := errors.New("boom")
+	err := errors.New("different")
+
+	hammy.New(aSpy).Is(hammy.ErrorIs(err, target))
+	aSpy.HadErrorContaining(t, "got <different>, want error matching <boom>")
+}
+
+func Test_error_as_success(t *testing.T) {
+	err := fmt.Errorf("wrapped: %w", &customError{message: "boom"})
+	var target *customError
+
+	hammy.New(t).Is(hammy.ErrorAs(err, &target))
+}
+
+func Test_error_as_failure(t *testing.T) {
+	aSpy := eye.Spy()
+	err := errors.New("boom")
+	var target *customError
+
+	hammy.New(aSpy).Is(hammy.ErrorAs(err, &target))
+	aSpy.HadErrorContaining(t, "want error assignable")
 }
