50a3ac41946039a14506fcfbf656f0ef2013830f
Nathan Fisher <nfisher@nvidia.com>
2026-05-12 10:53:53 -0400

    Add richer error assertions

--- hammy/error.go
+++ hammy/error.go
@@ -1,6 +1,10 @@
 package hammy
 
-import "errors"
+import (
+	"errors"
+	"regexp"
+	"strings"
+)
 
 func NilError(err error) AssertionMessage {
 	return Assert(err == nil, "got <%v>, want nil error", err)
@@ -10,10 +14,49 @@ func Error(err error) AssertionMessage {
 	return Assert(err != nil, "got %v, want error", err)
 }
 
+func EqualError(err error, expected string) AssertionMessage {
+	if err == nil {
+		return Assert(false, "got nil error, want error message <%s>", expected)
+	}
+	return Assert(err.Error() == expected, "got error message <%s>, want <%s>", err.Error(), expected)
+}
+
+func ErrorContains(err error, expected string) AssertionMessage {
+	if err == nil {
+		return Assert(false, "got nil error, want error containing <%s>", expected)
+	}
+	return Assert(strings.Contains(err.Error(), expected), "got error message <%s>, want containing <%s>", err.Error(), expected)
+}
+
+func ErrorMatchesRegexp(err error, pattern string) AssertionMessage {
+	if err == nil {
+		return Assert(false, "got nil error, want error matching regexp <%s>", pattern)
+	}
+
+	re, compileErr := regexp.Compile(pattern)
+	if compileErr != nil {
+		return Assert(false, "invalid regexp <%s>: %v", pattern, compileErr)
+	}
+	return Assert(re.MatchString(err.Error()), "got error message <%s>, want regexp <%s>", err.Error(), pattern)
+}
+
 func ErrorIs(err error, target error) AssertionMessage {
 	return Assert(errors.Is(err, target), "got <%v>, want error matching <%v>", err, target)
 }
 
+func NotErrorIs(err error, target error) AssertionMessage {
+	return Assert(!errors.Is(err, target), "got <%v>, want error not matching <%v>", err, target)
+}
+
 func ErrorAs[T any](err error, target *T) AssertionMessage {
 	return Assert(errors.As(err, target), "got <%v>, want error assignable to <%T>", err, target)
+}
+
+func NotErrorAs[T any](err error, target *T) AssertionMessage {
+	return Assert(!errors.As(err, target), "got <%v>, want error not assignable to <%T>", err, target)
+}
+
+func ErrorType[T error](err error) AssertionMessage {
+	var target T
+	return Assert(errors.As(err, &target), "got <%v>, want error assignable to <%T>", err, target)
 }
--- hammy/error_test.go
+++ hammy/error_test.go
@@ -28,6 +28,63 @@ func Test_is_error_failure(t *testing.T) {
 	aSpy.HadErrorContaining(t, "got <nil>, want error")
 }
 
+func Test_equal_error_success(t *testing.T) {
+	err := errors.New("hello world")
+
+	a.New(t).Is(a.EqualError(err, "hello world"))
+}
+
+func Test_equal_error_failure(t *testing.T) {
+	aSpy := eye.Spy()
+	err := errors.New("hello world")
+
+	a.New(aSpy).Is(a.EqualError(err, "hello"))
+	aSpy.HadErrorContaining(t, "got error message <hello world>, want <hello>")
+}
+
+func Test_equal_error_failure_on_nil(t *testing.T) {
+	aSpy := eye.Spy()
+
+	a.New(aSpy).Is(a.EqualError(nil, "hello"))
+	aSpy.HadErrorContaining(t, "got nil error, want error message <hello>")
+}
+
+func Test_error_contains_success(t *testing.T) {
+	err := errors.New("hello world")
+
+	a.New(t).Is(a.ErrorContains(err, "world"))
+}
+
+func Test_error_contains_failure(t *testing.T) {
+	aSpy := eye.Spy()
+	err := errors.New("hello world")
+
+	a.New(aSpy).Is(a.ErrorContains(err, "goodbye"))
+	aSpy.HadErrorContaining(t, "got error message <hello world>, want containing <goodbye>")
+}
+
+func Test_error_matches_regexp_success(t *testing.T) {
+	err := errors.New("request failed with status 503")
+
+	a.New(t).Is(a.ErrorMatchesRegexp(err, `status \d+`))
+}
+
+func Test_error_matches_regexp_failure(t *testing.T) {
+	aSpy := eye.Spy()
+	err := errors.New("request failed with status 503")
+
+	a.New(aSpy).Is(a.ErrorMatchesRegexp(err, `status [12]\d\d`))
+	aSpy.HadErrorContaining(t, "want regexp <status [12]\\d\\d>")
+}
+
+func Test_error_matches_regexp_invalid_pattern_failure(t *testing.T) {
+	aSpy := eye.Spy()
+	err := errors.New("hello world")
+
+	a.New(aSpy).Is(a.ErrorMatchesRegexp(err, `(`))
+	aSpy.HadErrorContaining(t, "invalid regexp <(>")
+}
+
 func Test_nil_error_success(t *testing.T) {
 	a.New(t).Is(a.NilError(nil))
 }
@@ -55,6 +112,22 @@ func Test_error_is_failure(t *testing.T) {
 	aSpy.HadErrorContaining(t, "got <different>, want error matching <boom>")
 }
 
+func Test_not_error_is_success(t *testing.T) {
+	target := errors.New("boom")
+	err := errors.New("different")
+
+	a.New(t).Is(a.NotErrorIs(err, target))
+}
+
+func Test_not_error_is_failure(t *testing.T) {
+	aSpy := eye.Spy()
+	target := errors.New("boom")
+	err := fmt.Errorf("wrapped: %w", target)
+
+	a.New(aSpy).Is(a.NotErrorIs(err, target))
+	aSpy.HadErrorContaining(t, "want error not matching <boom>")
+}
+
 func Test_error_as_success(t *testing.T) {
 	err := fmt.Errorf("wrapped: %w", &customError{message: "boom"})
 	var target *customError
@@ -68,5 +141,35 @@ func Test_error_as_failure(t *testing.T) {
 	var target *customError
 
 	a.New(aSpy).Is(a.ErrorAs(err, &target))
+	aSpy.HadErrorContaining(t, "want error assignable")
+}
+
+func Test_not_error_as_success(t *testing.T) {
+	err := errors.New("boom")
+	var target *customError
+
+	a.New(t).Is(a.NotErrorAs(err, &target))
+}
+
+func Test_not_error_as_failure(t *testing.T) {
+	aSpy := eye.Spy()
+	err := fmt.Errorf("wrapped: %w", &customError{message: "boom"})
+	var target *customError
+
+	a.New(aSpy).Is(a.NotErrorAs(err, &target))
+	aSpy.HadErrorContaining(t, "want error not assignable")
+}
+
+func Test_error_type_success(t *testing.T) {
+	err := fmt.Errorf("wrapped: %w", &customError{message: "boom"})
+
+	a.New(t).Is(a.ErrorType[*customError](err))
+}
+
+func Test_error_type_failure(t *testing.T) {
+	aSpy := eye.Spy()
+	err := errors.New("boom")
+
+	a.New(aSpy).Is(a.ErrorType[*customError](err))
 	aSpy.HadErrorContaining(t, "want error assignable")
 }
