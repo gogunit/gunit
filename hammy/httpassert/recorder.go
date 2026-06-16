ee460eede948a8854bbc3d399bb267bd11808beb
Nathan Fisher <nfisher@junctionbox.ca>
2026-06-16 20:11:55 +0000

    Make recorder assertions self-contained

--- hammy/httpassert/recorder.go
+++ hammy/httpassert/recorder.go
@@ -3,6 +3,8 @@ package httpassert
 import (
 	"net/http"
 	"net/http/httptest"
+	"regexp"
+	"strings"
 
 	"github.com/gogunit/gunit/hammy"
 )
@@ -23,7 +25,7 @@ func (r *RecorderAssert) Status(expected int) hammy.AssertionMessage {
 	if !result.IsSuccessful {
 		return hammy.Assert(false, "got nil response recorder, wanted status <%d>", expected)
 	}
-	return Status(resp, expected)
+	return hammy.Assert(resp.StatusCode == expected, "got status <%d>, wanted <%d>", resp.StatusCode, expected)
 }
 
 // StatusInRange asserts that the recorded response status is between min and max, inclusive.
@@ -35,7 +37,7 @@ func (r *RecorderAssert) StatusInRange(min, max int) hammy.AssertionMessage {
 	if !result.IsSuccessful {
 		return hammy.Assert(false, "got nil response recorder, wanted status in range <%d..%d>", min, max)
 	}
-	return StatusInRange(resp, min, max)
+	return hammy.Assert(resp.StatusCode >= min && resp.StatusCode <= max, "got status <%d>, wanted in range <%d..%d>", resp.StatusCode, min, max)
 }
 
 // Header asserts that the recorded response header equals expected.
@@ -44,7 +46,8 @@ func (r *RecorderAssert) Header(key, expected string) hammy.AssertionMessage {
 	if !result.IsSuccessful {
 		return hammy.Assert(false, "got nil response recorder, wanted header <%s> equal to <%s>", key, expected)
 	}
-	return Header(resp, key, expected)
+	actual := resp.Header.Get(key)
+	return hammy.Assert(actual == expected, "got header <%s>=<%s>, wanted <%s>", key, actual, expected)
 }
 
 // HeaderContains asserts that the recorded response header contains expected.
@@ -53,34 +56,39 @@ func (r *RecorderAssert) HeaderContains(key, expected string) hammy.AssertionMes
 	if !result.IsSuccessful {
 		return hammy.Assert(false, "got nil response recorder, wanted header <%s> containing <%s>", key, expected)
 	}
-	return HeaderContains(resp, key, expected)
+	actual := resp.Header.Get(key)
+	return hammy.Assert(strings.Contains(actual, expected), "got header <%s>=<%s>, wanted containing <%s>", key, actual, expected)
 }
 
 // BodyEqual asserts that the recorded response body equals expected.
 func (r *RecorderAssert) BodyEqual(expected string) hammy.AssertionMessage {
-	resp, result := r.response()
+	actual, result := r.body()
 	if !result.IsSuccessful {
 		return hammy.Assert(false, "got nil response recorder, wanted response body")
 	}
-	return BodyEqual(resp, expected)
+	return hammy.Assert(actual == expected, "got body <%s>, wanted equal to <%s>", actual, expected)
 }
 
 // BodyContains asserts that the recorded response body contains expected.
 func (r *RecorderAssert) BodyContains(expected string) hammy.AssertionMessage {
-	resp, result := r.response()
+	actual, result := r.body()
 	if !result.IsSuccessful {
 		return hammy.Assert(false, "got nil response recorder, wanted response body")
 	}
-	return BodyContains(resp, expected)
+	return hammy.Assert(strings.Contains(actual, expected), "got body <%s>, wanted containing <%s>", actual, expected)
 }
 
 // BodyMatchesRegexp asserts that the recorded response body matches pattern.
 func (r *RecorderAssert) BodyMatchesRegexp(pattern string) hammy.AssertionMessage {
-	resp, result := r.response()
+	actual, result := r.body()
 	if !result.IsSuccessful {
 		return hammy.Assert(false, "got nil response recorder, wanted response body")
 	}
-	return BodyMatchesRegexp(resp, pattern)
+	re, err := regexp.Compile(pattern)
+	if err != nil {
+		return hammy.Assert(false, "invalid regexp <%s>: %v", pattern, err)
+	}
+	return hammy.Assert(re.MatchString(actual), "got body <%s>, wanted regexp <%s>", actual, pattern)
 }
 
 func (r *RecorderAssert) response() (*http.Response, hammy.AssertionMessage) {
@@ -88,4 +96,14 @@ func (r *RecorderAssert) response() (*http.Response, hammy.AssertionMessage) {
 		return nil, hammy.Assert(false, "got nil response recorder")
 	}
 	return r.actual.Result(), hammy.Assert(true, "got response recorder")
+}
+
+func (r *RecorderAssert) body() (string, hammy.AssertionMessage) {
+	if r == nil || r.actual == nil {
+		return "", hammy.Assert(false, "got nil response recorder")
+	}
+	if r.actual.Body == nil {
+		return "", hammy.Assert(true, "got response recorder body")
+	}
+	return r.actual.Body.String(), hammy.Assert(true, "got response recorder body")
 }
